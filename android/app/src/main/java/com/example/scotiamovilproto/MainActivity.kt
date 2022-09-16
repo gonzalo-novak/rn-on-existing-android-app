32c297738f4b7694d577bd00df21d531c26a4d3c
gonzalo-novak <gonzalo.stergo@monstertrap.art>
2022-09-16 16:44:23 -0500

    chore: react native integration with an existing android application structure

---
+++ android/app/src/main/java/com/example/scotiamovilproto/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.scotiamovilproto
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.scotiamovilproto.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    /** Called when the user touches the button */
+    fun sendMessage(view: View) {
+        // Set up the intent
+        val i = Intent(applicationContext, MyReactActivity::class.java)
+        // Launch It
+        startActivity(i)
+    }
+}
